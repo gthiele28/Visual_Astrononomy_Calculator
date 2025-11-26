914153fcd50841fad86b827954062cdf9b23bf64
grant <gthiele2028@berkeley.edu>
2025-11-25 16:08:16 -0800

    Web scraping completed minus the 3 error-causing entries, must find and append

---
+++ NGCScraper.py
@@ -0,0 +1,73 @@
+from urllib.request import urlopen
+import json
+
+demo_url = "https://www.go-astronomy.com/ngc.php?ID=1"
+
+def extract_data(url):
+    '''Extract data from a url passed in,
+       return dictionary with data and labels
+       from the table'''
+    
+    page = urlopen(url)
+
+    html_raw = page.read()
+
+    html = html_raw.decode("utf-8")
+
+    table_start = html.find("<tbody>")
+    table_end = html.find("</tbody>")
+
+    table = html[table_start:table_end]
+
+    data = {}
+
+    row_ids = "<tr>"
+    row_idf = "</tr>"
+
+    label_ids = "<strong>"
+    label_idf = "</strong>"
+
+    label_ds = "</td><td>"
+    label_df = "</td></tr>"
+
+    while table.find(row_ids) != -1:
+
+        curr = table[table.find(row_ids):table.find(row_idf) + len(row_idf)]
+
+        key = curr[curr.find(label_ids) + len(label_ids): curr.find(label_idf) - 1]
+        val = curr[curr.find(label_ds) + len(label_ds): curr.find(label_df)]
+
+        data[key] = val
+
+        table = table[table.find(row_idf) + len(row_idf):]
+    
+    return data
+
+
+
+
+#To avoid errors, I tried implementing try/catch which will block
+#my code from crashing when I find � characters, and will dump
+#a list of all i values into a separate error-location file
+
+#Runtime roughly 1 hour, try/catch triggered 3 times total
+#must update those 3 in a separate script and append to end of raw_NGC
+
+raw_data = open("ngc_data.json", 'w') #different filename than raw_NGC(which has actual results) to prevent me from overwriting it
+error_locs = open("error_nums.txt", "w")
+fail_count = 0
+
+for i in range(1,8374): #multiple loops used, ignore these values
+    url_x = "https://www.go-astronomy.com/ngc.php?ID=" + str(i)
+
+    try:
+        collected = extract_data(url_x)
+        json.dump(collected, raw_data)
+        raw_data.write('\n')
+        print("Catalogued up to " + str(i))
+    except UnicodeDecodeError:
+        fail_count += 1
+        error_locs.write(str(i) + '\n') #newline so i can use .readlines() to split back to list later
+        print("UnicodeDecodeError at " + str(i))
+
+print("Done!")
