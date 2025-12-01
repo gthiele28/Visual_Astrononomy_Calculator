19db8e9a95153b5650060d6e1717d5140158b50d
grant <gthiele2028@berkeley.edu>
2025-12-01 11:47:46 -0800

    included list of required libraries and set of commands to install them

--- full_process.py
+++ full_process.py
@@ -11,6 +11,19 @@ datapaths = ["Data Collection/Complete Datasets/raw_NGC.json", "Data Collection/
 #AN ARM-64 MAC AND CHROME 142, JUST LEAVE THIS AND IT WILL WORK
 path = "chromedriver-mac-arm64/chromedriver"
 
+#TODO: LIST OF COMMANDS TO RUN TO INSTALL REQUIRED LIBRARIES
+#pip install json
+#pip install urllib
+#pip install time
+#pip install geocoder
+#pip install datetime
+#pip install numpy
+#pip install ast
+#pip install astropy
+#pip install timezonefinder
+#pip install zoneinfo
+#pip install selenium
+
 getInputs.getInputs()
 WeatherScraper.full_weather_scrape(path)
 Calculator.full_calc("Weather/weather_info.txt", "Inputs/date.txt", "Inputs/location.txt", "Inputs/telescope.txt", datapaths)
